c8efce555dcd69318164a2f9440e858b8880ffee
DESTINYMEDIA1 <odoruyidestiny2@gmail.com>
2025-03-03 17:40:12 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'DESTINY',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "DESTINY MEDIA",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '07061252041', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
